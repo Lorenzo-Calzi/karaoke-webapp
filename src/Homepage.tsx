9b6ab455e75758a5bb1b0e741005827e3ca81f97
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-06-12 12:16:03 +0200

    corretti font e spaziature

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -16,8 +16,11 @@ function Homepage() {
 
     return (
         <div className="homepage container">
-            <img className="logo" src="/images/karaoke_logo_2.png" alt="logo karaoke" />
-            <p className="title">Benvenuto! Scegli un'opzione:</p>
+            <img className="logo" src="/images/karaoke_logo_3.png" alt="logo karaoke" />
+            <div>
+                <h3 className="title">È il tuo momento!</h3>
+                <h3 className="title">Scegli un’opzione:</h3>
+            </div>
 
             <div className="buttons_list">
                 <button
