ed5f5b3acd086fce16e262f5150645753fc8fdb0
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-06-12 14:54:43 +0200

    test bug scroll

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-import Layout from "./Layout";
+// import Layout from "./Layout";
 import Homepage from "./Homepage";
-import ListaCanzoni from "./pages/ListaCanzoni";
-import ConsigliaUnaCanzone from "./pages/ConsigliaUnaCanzone";
-import Social from "./pages/Social";
+// import ListaCanzoni from "./pages/ListaCanzoni";
+// import ConsigliaUnaCanzone from "./pages/ConsigliaUnaCanzone";
+// import Social from "./pages/Social";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route element={<Layout />}>
+                <Route path="/" element={<Homepage />} />
+                {/* <Route element={<Layout />}>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/canzoni" element={<ListaCanzoni />} />
                     <Route path="/consigliaci" element={<ConsigliaUnaCanzone />} />
                     <Route path="/social" element={<Social />} />
-                </Route>
+                </Route> */}
             </Routes>
         </BrowserRouter>
     </StrictMode>
