fb405dc2ed4e59b93aecf0694549079b77b40ece
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-06-12 14:41:44 +0200

    test bug scroll

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
 import { useLocation, Outlet } from "react-router-dom";
 import "./layout.scss";
 
 function Layout() {
     const location = useLocation();
-
-    useEffect(() => {
-        const setVh = () => {
-            const vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty("--vh", `${vh}px`);
-        };
-        setVh();
-        window.addEventListener("resize", setVh);
-        return () => window.removeEventListener("resize", setVh);
-    }, []);
 
     const getBgClass = () => {
         const path = location.pathname;
