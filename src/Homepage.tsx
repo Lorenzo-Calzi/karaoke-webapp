fed8c1ed69de851719e2fd886bb7a49765bf120d
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-06-17 17:44:26 +0200

    modificato layout e stile dei bottoni nell'homepage

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -13,25 +13,33 @@ function Homepage() {
     return (
         <div className="homepage container">
             <img className="logo" src="/images/karaoke_logo_3.png" alt="logo karaoke" />
-            <div>
+            {/* <div>
                 <h3 className="title">È il tuo momento!</h3>
                 <h3 className="title">Scegli un’opzione:</h3>
-            </div>
+            </div> */}
 
             <div className="buttons_list">
                 <button onClick={() => handleClick("/canzoni")}>
                     <i className="fa-solid fa-list-ul"></i>
-                    <span>KARAOKE: i nostri consigli</span>
+                    <div className="button_content">
+                        <span className="button_title">KARAOKE</span>
+                        <p className="button_description">Le canzoni che ti consigliamo</p>
+                    </div>
                 </button>
 
                 <button onClick={() => handleClick("/consigliaci")}>
                     <i className="fa-solid fa-lightbulb"></i>
-                    <span>DJ SET: consiglia una canzone</span>
+                    <div className="button_content">
+                        <span className="button_title">DJ SET</span>
+                        <p className="button_description">Vota le canzoni che vuoi ballare</p>
+                    </div>
                 </button>
-
                 <button onClick={() => handleClick("/social")}>
                     <i className="fa-brands fa-instagram"></i>
-                    <span>Social</span>
+                    <div className="button_content">
+                        <span className="button_title">SOCIAL</span>
+                        <p className="button_description">I nostri profili Instagram</p>
+                    </div>
                 </button>
             </div>
         </div>
