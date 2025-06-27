bb69fedbd6ffc4f86927cfe407f719daa6f0dfd3
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-06-27 12:25:28 +0200

    modificati logica che mostra i prossimi eventi

--- src/pages/Calendario/Calendario.tsx
+++ src/pages/Calendario/Calendario.tsx
@@ -3,11 +3,17 @@ import EventCard from "../../components/EventCard/EventCard";
 import "./calendario.scss";
 
 export default function Calendario() {
+    const now = new Date();
+    const eventiFuturi = eventi.filter(event => {
+        const startDate = new Date(event.start);
+        return startDate >= now;
+    });
+
     return (
         <div className="calendario container">
             <h2 className="title">Prossimi Eventi</h2>
             <div className="calendario-list">
-                {eventi.map((event, idx) => (
+                {eventiFuturi.map((event, idx) => (
                     <EventCard key={idx} event={event} />
                 ))}
             </div>
