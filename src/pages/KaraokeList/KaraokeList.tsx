871ad2827b124797da7d3a56b288e83277ee8607
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-10-07 18:47:30 +0200

    cambiato delay

--- src/pages/KaraokeList/KaraokeList.tsx
+++ src/pages/KaraokeList/KaraokeList.tsx
@@ -190,7 +190,7 @@ export default function KaraokeList() {
     const [showResults, setShowResults] = useState(false);
     const skipNextSearch = useRef(false);
 
-    const BLUR_DELAY_MS = 100;
+    const BLUR_DELAY_MS = 200;
     const blurTimer = useRef<number | null>(null);
 
     const fetchList = async () => {
