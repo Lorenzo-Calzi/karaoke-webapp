f10a809bc4e408a51e9e712af319660b7222077b
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2025-10-09 14:53:31 +0200

    modificati tempi dei popup

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -3,23 +3,23 @@ import { toast } from "react-toastify";
 export const showSuccess = (message: string) =>
     toast.success(message, {
         position: "top-center",
-        autoClose: 3000
+        autoClose: 1000
     });
 
 export const showError = (message: string) =>
     toast.error(message, {
         position: "top-center",
-        autoClose: 4000
+        autoClose: 1000
     });
 
 export const showInfo = (message: string) =>
     toast.info(message, {
         position: "top-center",
-        autoClose: 3000
+        autoClose: 1000
     });
 
 export const showWarning = (message: string) =>
     toast.warn(message, {
         position: "top-center",
-        autoClose: 3500
+        autoClose: 1000
     });
